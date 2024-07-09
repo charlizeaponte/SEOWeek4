640b12f5c46b8d64f7cfe997f7386e2e969f6020
charlizeaponte <charlizeaponte@yahoo.com>
2024-07-09 15:05:41 -0400

    "added first three screens"

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.library") version "8.2.1" apply false
+}
